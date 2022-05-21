e3f27d4d4208c722e3dc8fd1e74db2c1a59a70b3
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-05-21 13:26:54 +0600

    Add java file

---
+++ First-Java-Program.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args){
+    // write your code.
+        System.out.println("Hello World");
+        System.out.println("Hello, Java");
+    }
+}
